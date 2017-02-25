775e448518d7bdde7fd5f8685c3f7506b9092b56
Tsubasa <xxx.tubasa0226@gmail.com>
2017-02-25 11:15:46 +0900

    Allow to disable syncing scroll in side-by-side mode via options

--- src/js/simplemde.js
+++ src/js/simplemde.js
@@ -1667,6 +1667,7 @@ SimpleMDE.prototype.createSideBySide = function() {
 		wrapper.parentNode.insertBefore(preview, wrapper.nextSibling);
 	}
 
+	if(this.options.syncPreviewSideScroll === false) return preview;
 	// Syncs scroll  editor -> preview
 	var cScroll = false;
 	var pScroll = false;
