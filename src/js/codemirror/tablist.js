de26368ad1d01581f7e6611004f9b091262d6a7e
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2017-12-05 11:51:36 +0100

    Set indent style to 4 spaces

--- src/js/codemirror/tablist.js
+++ src/js/codemirror/tablist.js
@@ -4,41 +4,41 @@
 var CodeMirror = require("codemirror");
 
 CodeMirror.commands.tabAndIndentMarkdownList = function (cm) {
-	var ranges = cm.listSelections();
-	var pos = ranges[0].head;
-	var eolState = cm.getStateAfter(pos.line);
-	var inList = eolState.list !== false;
+    var ranges = cm.listSelections();
+    var pos = ranges[0].head;
+    var eolState = cm.getStateAfter(pos.line);
+    var inList = eolState.list !== false;
 
-	if (inList) {
-		cm.execCommand("indentMore");
-		return;
-	}
+    if (inList) {
+        cm.execCommand("indentMore");
+        return;
+    }
 
-	if (cm.options.indentWithTabs) {
-		cm.execCommand("insertTab");
-	}
-	else {
-		var spaces = Array(cm.options.tabSize + 1).join(" ");
-		cm.replaceSelection(spaces);
-	}
+    if (cm.options.indentWithTabs) {
+        cm.execCommand("insertTab");
+    }
+    else {
+        var spaces = Array(cm.options.tabSize + 1).join(" ");
+        cm.replaceSelection(spaces);
+    }
 };
 
 CodeMirror.commands.shiftTabAndUnindentMarkdownList = function (cm) {
-	var ranges = cm.listSelections();
-	var pos = ranges[0].head;
-	var eolState = cm.getStateAfter(pos.line);
-	var inList = eolState.list !== false;
+    var ranges = cm.listSelections();
+    var pos = ranges[0].head;
+    var eolState = cm.getStateAfter(pos.line);
+    var inList = eolState.list !== false;
 
-	if (inList) {
-		cm.execCommand("indentLess");
-		return;
-	}
+    if (inList) {
+        cm.execCommand("indentLess");
+        return;
+    }
 
-	if (cm.options.indentWithTabs) {
-		cm.execCommand("insertTab");
-	}
-	else {
-		var spaces = Array(cm.options.tabSize + 1).join(" ");
-		cm.replaceSelection(spaces);
-	}
+    if (cm.options.indentWithTabs) {
+        cm.execCommand("insertTab");
+    }
+    else {
+        var spaces = Array(cm.options.tabSize + 1).join(" ");
+        cm.replaceSelection(spaces);
+    }
 };
