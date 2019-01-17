5b01fba882d9e1d4a3dd9092fd4511bbbc892502
Jeroen Akkerman <jeroen_akkerman1@hotmail.com>
2019-01-17 11:12:40 +0100

    Added more tests for the types file.

--- types/easymde-test.ts
+++ types/easymde-test.ts
@@ -1,4 +1,5 @@
-new EasyMDE({
+// Create new instance
+const editor = new EasyMDE({
     autoDownloadFontAwesome: false,
     element: document.getElementById("mdEditor")!,
     hideIcons: ["side-by-side", "fullscreen"],
@@ -6,5 +7,20 @@ new EasyMDE({
         drawTable: "Cmd-Alt-T",
         toggleFullScreen: null
     },
-    spellChecker: false
+    spellChecker: false,
+    onToggleFullScreen: (full: boolean) => { console.log('FullscreenToggled', full); },
+    theme: 'someOtherTheme',
 });
+
+// Editor functions
+const value = editor.value() as string;
+editor.value(value.toUpperCase());
+
+const sbs = editor.isSideBySideActive() as boolean;
+const fullscreen = editor.isFullscreenActive() as boolean;
+
+// Access to codemirror object
+editor.codemirror.setOption('readOnly', true);
+
+// Static properties
+EasyMDE.toggleItalic = (editor: EasyMDE) => { console.log('SomeButtonOverride'); };
